fc7dc11f7d2bd872dbf8ff6a15d781112f67816b
Keagan O'Donoghue <k3agan@gmail.com>
2020-10-24 02:12:08 -0700

    ''

---
+++ hackerRank_Solutions/merge_the_tools.py
@@ -0,0 +1,9 @@
+def merge_the_tools(string,k):
+    while string:
+        s = string[0:k]
+        seen = ''
+        for c in s:
+            if c not in seen:
+                seen += c
+        print(seen)
+        string = string[k:]
